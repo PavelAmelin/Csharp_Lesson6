fcbdd9c788fbdf665ee7203567a2539bd32fdaac
Pavel <sivec_p@mail.ru>
2023-01-29 20:18:11 +0300

    Add Task41 CountPositiveCars

---
+++ Task41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите количество чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int cnt = 0;
+for (int i = 0; i < M; i++)
+{
+    int ch = Convert.ToInt32(Console.ReadLine());
+    if (ch > 0)
+        cnt += 1;
+}
+Console.WriteLine($"Количество чисел больше нуля - {cnt}");
